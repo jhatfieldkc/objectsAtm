4a170181ddd606f4a4f4dc70f0806dcb56e3b7ba
jhatfieldkc <jhatfieldkc@gmail.com>
2020-12-16 15:06:11 -0600

    Update main.js

---
+++ main.js
@@ -0,0 +1,15 @@
+/*
+Object: account
+Create new account - account.create()
+Type[checking/savings]
+Balance[balance]
+Create Pin[createPin]
+Change Pin[changePin]
+
+
+Object: math formulas
+Opening Deposit[openDeposit$amount+]
+Deposit[balance + depositAmount]
+Widthdrawl[balance - depositAmount]
+Transfer[balance - moveAmount]
+*/
